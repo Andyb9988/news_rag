a72da334c199e896ebfef1c3aaf9877f531b3cc3
Andrew Bogle <andyb9988@hotmail.co.uk>
2024-08-21 16:21:53 +0100

    holder for unittests

---
+++ tests/test_placeholder.py
@@ -0,0 +1,11 @@
+import unittest
+
+
+class TestMyFeature(unittest.TestCase):
+    def test_placeholder(self):
+        """Placeholder test that will fail until implemented."""
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
